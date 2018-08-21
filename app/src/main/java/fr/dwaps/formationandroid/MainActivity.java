d75c5ed4b05e684fc09b6dee6299d74f96bcd0cf
Michael Cornillon <contact@dwaps.fr>
2018-08-21 22:40:03 +0200

    Stockage interne : écriture dans un fichier

--- app/src/main/java/fr/dwaps/formationandroid/MainActivity.java
+++ app/src/main/java/fr/dwaps/formationandroid/MainActivity.java
@@ -9,14 +9,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
-    private static final String PREF_FILE_INFOS = "infos";
-    private static final String PREF_FILE_THEME = "theme";
-    private final String PREF_COLOR = "Ma couleur";
-    private final String PREF_AGE = "Mon age";
-    private final String PREF_EMAIL = "Mon email";
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
-    SharedPreferences prefInfos, prefTheme;
+public class MainActivity extends Activity {
+    final String FILENAME = "mon-fichier.txt";
+
     EditText et;
 
     @Override
@@ -24,31 +22,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        prefInfos = getSharedPreferences(PREF_FILE_INFOS, MODE_PRIVATE);
-        prefTheme = getSharedPreferences(PREF_FILE_THEME, MODE_PRIVATE);
         et = findViewById(R.id.et);
     }
 
-    public void save(View v) {
-        prefTheme.edit().putString(PREF_COLOR, et.getText().toString()).apply();
+    public void save(View v) throws Exception {
+        FileOutputStream fos = openFileOutput(FILENAME, MODE_PRIVATE);
+
+        if (fos != null) {
+            fos.write(et.getText().toString().getBytes());
+            fos.close();
+        }
     }
 
     public void load(View v) {
-        et.setText(prefTheme.getString(PREF_COLOR, ""));
     }
 
     public void delete(View v) {
-        prefTheme.edit().remove(PREF_COLOR).apply();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        prefTheme.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
-            @Override
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-                et.setText("La préférence a bien été mise à jour");
-            }
-        });
     }
 }
