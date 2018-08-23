a1b4055c533a90330b869863342c17f76d86296f
Michael Cornillon <contact@dwaps.fr>
2018-08-23 17:21:03 +0200

    Stockage externe : accès aux fichiers de l'application

--- app/src/main/java/fr/dwaps/formationandroid/MainActivity.java
+++ app/src/main/java/fr/dwaps/formationandroid/MainActivity.java
@@ -1,18 +1,23 @@
 package fr.dwaps.formationandroid;
 
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
+import android.os.Environment;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static android.os.Environment.getExternalStorageDirectory;
+import static android.os.Environment.getExternalStoragePublicDirectory;
+import static android.os.Environment.getExternalStorageState;
 
 public class MainActivity extends Activity {
+    final String LOG_TAG = "DWAPS FORMATION";
     final String FILENAME = "mon-fichier.txt";
 
     EditText et;
@@ -25,30 +30,64 @@ public class MainActivity extends Activity {
         et = findViewById(R.id.et);
     }
 
-    public void save(View v) throws Exception {
-        FileOutputStream fos = openFileOutput(FILENAME, MODE_PRIVATE);
+    public void save(View v) {
+        String storageState = getExternalStorageState();
 
-        if (fos != null) {
-            fos.write(et.getText().toString().getBytes());
-            fos.close();
+        switch (storageState) {
+            case Environment.MEDIA_MOUNTED:
+                File extDir = getExternalFilesDir(null); // A la racine du dossier files de l'app
+                File rootDir = getExternalStorageDirectory();
+                File publicRootDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+                FileWriter writer = null;
+                File dir = new File(publicRootDir.getAbsolutePath()+"/DossierTest");
+                dir.mkdirs();
+                File f = new File(dir, "hello.txt");
+
+                try {
+                    writer = new FileWriter(f);
+                    writer.write("Salut les cocos !");
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    if (writer != null) {
+                        try {
+                            writer.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                break;
+            case Environment.MEDIA_MOUNTED_READ_ONLY:
+                // TODO
+                break;
+            case Environment.MEDIA_REMOVED:
+                // TODO
+                break;
+            default:
         }
     }
 
-    public void load(View v) throws Exception {
-        FileInputStream fis = openFileInput(FILENAME);
-        StringBuilder sb = new StringBuilder();
-        byte[] buffer = new byte[1024];
-
-        if (fis != null) {
-            while(fis.read(buffer) != -1) {
-                sb.append(new String(buffer));
-            }
-
-            et.setText(sb.toString());
-            fis.close();
+    public void load(View v) {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILENAME);
+            fis.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Fichier inexistant !", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void delete(View v) {
+        String dir = getFilesDir().getAbsolutePath();
+        File file = new File(dir, FILENAME);
+        if (file.delete()) {
+            Toast.makeText(this, "Suppression du fichier", Toast.LENGTH_SHORT).show();
+        }
     }
 }
